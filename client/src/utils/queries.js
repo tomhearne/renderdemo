0bfd8267b617345f66d236b44e84ed8a86709422
Tom Hearne <tomrpstudios@gmail.com>
2024-06-07 17:46:39 -0500

    Add files via upload

---
+++ client/src/utils/queries.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_PROFILES = gql`
+  query allProfiles {
+    profiles {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const QUERY_QUESTIONS = gql`
+query Questions {
+    questions {
+        title
+        body
+        _id
+        user {
+          firstName
+          lastName
+        }
+      }
+    }
+`;
+
+export const QUERY_SINGLE_PROFILE = gql`
+  query singleProfile($profileId: ID!) {
+    profile(profileId: $profileId) {
+      _id
+      name
+      skills
+    }
+  }
+`;
+
+export const QUERY_ME = gql`
+  query me {
+    me {
+      _id
+      name
+      skills
+    }
+  }
+`;
